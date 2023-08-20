ec62429a8595321d8d3b366fd92e3870e4a83971
Jhon Darwin Grisales Giraldo <darwingrisalesg@gmail.com>
2023-08-20 15:39:41 -0500

    refactor: modify class to a type

--- src/domain/models/movie/movie.ts
+++ src/domain/models/movie/movie.ts
@@ -1,23 +1,11 @@
 export class Movie {
-  private readonly id: string;
-  private name: string;
-  private authorEmail: string;
+  id: string;
+  name: string;
+  authorEmail: string;
 
   constructor(id: string, name: string, authorEmail: string) {
     this.id = id;
     this.name = name;
     this.authorEmail = authorEmail;
-  }
-
-  getIdValue() {
-    return this.id;
-  }
-
-  getNameValue() {
-    return this.name;
-  }
-
-  getEmailValue() {
-    return this.authorEmail;
   }
 }
--- src/domain/models/user/user.ts
+++ src/domain/models/user/user.ts
@@ -1,29 +1,13 @@
 export class User {
-  private readonly id: string;
-  private name: string;
-  private age: number;
-  private email: string;
+  id: string;
+  name: string;
+  age: number;
+  email: string;
 
   constructor(id: string, name: string, age: number, email: string) {
     this.id = id;
     this.name = name;
     this.age = age;
     this.email = email;
-  }
-
-  getIdValue() {
-    return this.id;
-  }
-
-  getNameValue() {
-    return this.name;
-  }
-
-  getAgeValue() {
-    return this.age;
-  }
-
-  getEmailValue() {
-    return this.email;
   }
 }
